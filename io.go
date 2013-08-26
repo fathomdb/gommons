d13778d9951b90dcd0b582669333921b62113248
Justin Santa Barbara <justin@fathomdb.com>
2013-08-25 23:25:08 -0700

    Create ListDirectoryNames, to retrieve directory contents by name

--- io.go
+++ io.go
@@ -1,76 +1,117 @@
 package gommons
 
 import (
-    "io/ioutil"
-    "log"
-    "os"
-    "syscall"
+	"io/ioutil"
+	"log"
+	"os"
+	"sort"
+	"syscall"
 )
 
 func StatIfExists(path string) (stat os.FileInfo, err error) {
-    stat, err = os.Stat(path)
-    if err != nil {
-        if os.IsNotExist(err) {
-            return nil, nil
-        } else {
-            return nil, err
-        }
-    }
-    return stat, nil
+	stat, err = os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, nil
+		} else {
+			return nil, err
+		}
+	}
+	return stat, nil
 }
 
 func FileExists(path string) (exists bool, err error) {
-    stat, err := StatIfExists(path)
-    if err != nil {
-        return false, err
-    }
-    return stat != nil, nil
+	stat, err := StatIfExists(path)
+	if err != nil {
+		return false, err
+	}
+	return stat != nil, nil
 }
 
 func IsDirectory(path string) (exists bool, err error) {
-    stat, err := StatIfExists(path)
-    if err != nil {
-        return false, err
-    }
-    if stat == nil {
-        return false, nil
-    }
-    return stat.IsDir(), nil
+	stat, err := StatIfExists(path)
+	if err != nil {
+		return false, err
+	}
+	if stat == nil {
+		return false, nil
+	}
+	return stat.IsDir(), nil
 }
 
 func ListDirectory(dirname string) (files []os.FileInfo, err error) {
-    f, err := os.Open(dirname)
-    if err != nil {
-        log.Printf("Cannot open dir %s\n", dirname)
-        return nil, err
-    }
-    files, err = f.Readdir(-1)
-    f.Close()
-    if err != nil {
-        log.Printf("Cannot read dir %s\n", dirname)
-        return nil, err
-    }
+	f, err := os.Open(dirname)
+	if err != nil {
+		log.Printf("Cannot open dir %s\n", dirname)
+		return nil, err
+	}
+	defer f.Close()
+	files, err = f.Readdir(-1)
+	if err != nil {
+		log.Printf("Cannot read dir %s\n", dirname)
+		return nil, err
+	}
 
-    return files, nil
+	return files, nil
 }
 
-func TryReadFile(path string, defaultValue string) (string, error) {
-    contents, err := ioutil.ReadFile(path)
-    if err != nil {
-        if patherr, ok := err.(*os.PathError); ok {
-            if syserr, ok := patherr.Err.(syscall.Errno); ok {
-                if syserr == 2 {
-                    return defaultValue, nil
-                }
+// Lists a directory; unlike ListDirectory this returns just the names, and sorts them
+func ListDirectoryNames(dirname string) (files []string, err error) {
+	f, err := os.Open(dirname)
+	if err != nil {
+		log.Printf("Cannot open dir %s\n", dirname)
+		return nil, err
+	}
+	defer f.Close()
+	files, err = f.Readdirnames(-1)
+	if err != nil {
+		log.Printf("Cannot read dir %s\n", dirname)
+		return nil, err
+	}
 
-                log.Printf("Error reading file.  code=%v\n", int(syserr))
-            } else {
-                log.Printf("Error reading file %T\n", patherr.Err)
-            }
-        } else {
-            log.Printf("Error reading file %T\n", err)
-        }
-        return "", err
-    }
-    return string(contents), nil
+	sort.Strings(files)
+
+	return files, nil
+}
+
+func TryReadTextFile(path string, defaultValue string) (string, error) {
+	contents, err := ioutil.ReadFile(path)
+	if err != nil {
+		if patherr, ok := err.(*os.PathError); ok {
+			if syserr, ok := patherr.Err.(syscall.Errno); ok {
+				if syserr == 2 {
+					return defaultValue, nil
+				}
+
+				log.Printf("Error reading file.  code=%v\n", int(syserr))
+			} else {
+				log.Printf("Error reading file %T\n", patherr.Err)
+			}
+		} else {
+			log.Printf("Error reading file %T\n", err)
+		}
+		return "", err
+	}
+	return string(contents), nil
+}
+
+func TryReadFile(path string) ([]byte, error) {
+	contents, err := ioutil.ReadFile(path)
+	if err != nil {
+		if patherr, ok := err.(*os.PathError); ok {
+			if syserr, ok := patherr.Err.(syscall.Errno); ok {
+				if syserr == 2 {
+					return nil, nil
+				}
+
+				log.Printf("Error reading file.  code=%v\n", int(syserr))
+			} else {
+				log.Printf("Error reading file %T\n", patherr.Err)
+			}
+		} else {
+			log.Printf("Error reading file %T\n", err)
+		}
+		return nil, err
+	}
+	return contents, nil
 }
--- utils.go
+++ utils.go
@@ -1,56 +1,59 @@
 package gommons
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "os"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
 )
 
 func DeleteFile(path string) error {
-    err := os.Remove(path)
-    if err != nil {
-        if os.IsNotExist(err) {
-            err = nil
-        }
-    }
-    return err
+	err := os.Remove(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = nil
+		}
+	}
+	return err
 }
 
 func IsSafeName(name string) bool {
-    for _, v := range name {
-        if (v < 'A' || v > 'Z') && (v < 'a' || v > 'z') && (v < '0' || v > '9') {
-            switch v {
-            case '_', '-':
-                continue
-            default:
-                return false
-            }
-        }
-    }
+	for _, v := range name {
+		if (v < 'A' || v > 'Z') && (v < 'a' || v > 'z') && (v < '0' || v > '9') {
+			switch v {
+			case '_', '-':
+				continue
+			default:
+				return false
+			}
+		}
+	}
 
-    return true
+	return true
 }
 
 func CheckSafeName(name string) (err error) {
-    if name == "" || !IsSafeName(name) {
-        return fmt.Errorf("Invalid name: %s", name)
-    }
-    return nil
+	if name == "" || !IsSafeName(name) {
+		return fmt.Errorf("Invalid name: %s", name)
+	}
+	return nil
 }
 
-func ReadJson(path string, dest interface{}) error {
-    bytes, err := ioutil.ReadFile(path)
-    if err != nil {
-        return err
-    }
+func ReadJson(path string, dest interface{}) (found bool, err error) {
+	bytes, err := TryReadFile(path)
+	if err != nil {
+		return false, err
+	}
 
-    err = json.Unmarshal(bytes, dest)
-    if err != nil {
-        log.Printf("Invalid JSON in file %s", path)
-        return err
-    }
+	if bytes == nil {
+		return false, nil
+	}
 
-    return nil
+	err = json.Unmarshal(bytes, dest)
+	if err != nil {
+		log.Printf("Invalid JSON in file %s", path)
+		return false, err
+	}
+
+	return true, nil
 }
